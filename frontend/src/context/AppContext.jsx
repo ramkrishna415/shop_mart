b8d54626af0122c95f9035a171ec71a147472586
Ram krishna <goluupadhyay600@gmail.com>
2025-11-01 16:59:13 +0530

    some changes

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -3,8 +3,10 @@ import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL || "https://shop-mart-emnh.onrender.com",
-    // "http://localhost:5000"
+    baseURL:"https://shop-mart-emnh.onrender.com",
+    
+     
+    //  import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
